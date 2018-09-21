82512f75d8a149c4d07303846e0c89c11258cb43
Manibala Sinha <manibalak@gmail.com>
2018-09-21 12:48:30 -0400

    Intent Service: Communicating over the web.

--- app/src/main/java/com/example/android/intentservice/MainActivity.java
+++ app/src/main/java/com/example/android/intentservice/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.intentservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +16,32 @@ import com.example.android.intentservice.services.MyService;
 public class MainActivity extends AppCompatActivity {
     private static final String JSON_URL="http://560057.youcanlearnit.net/services/json/itemsfeed.php";
     TextView output;
+    //step6.
+    //listening architecture
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //message received MY_SERVICE_PAYLOAD
+            String message = intent.getStringExtra(MyService.MY_SERVICE_PAYLOAD);
+            output.append(message + "\n");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         output = (TextView) findViewById(R.id.output);
+        //step7
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mBroadcastReceiver, new IntentFilter(MyService.MY_SERVICE_MESSAGE));
     }
+//step 8
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(mBroadcastReceiver);
+    }
+
     public void runClickHandler(View view){
         //step3 pass the uri
         Intent intent = new Intent(this, MyService.class);
--- app/src/main/java/com/example/android/intentservice/services/MyService.java
+++ app/src/main/java/com/example/android/intentservice/services/MyService.java
@@ -4,12 +4,17 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 
 public class MyService extends IntentService {
 
     public  static final String TAG="MyService";
+    //one for listening and other for message itself
+    public static final String MY_SERVICE_MESSAGE= "myServiceMessage";
+    public static final String MY_SERVICE_PAYLOAD= "myServicePayload";
+
     //step2 constructor method
     public MyService() {
         //satisfy the rquirement of class by passing the name of class
@@ -27,6 +32,11 @@ public class MyService extends IntentService {
         } catch (InterruptedException e){
             e.printStackTrace();
         }
+        //step5
+        Intent messageIntent = new Intent(MY_SERVICE_MESSAGE);
+        messageIntent.putExtra(MY_SERVICE_PAYLOAD, "Service all done!");
+        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(getApplicationContext());
+        manager.sendBroadcast(messageIntent);
     }
 
     @Override
